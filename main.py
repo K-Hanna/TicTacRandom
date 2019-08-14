6001172da7e9ebb9a1adc4bda26c2de5c2087b06
Dong Ha Kotwa <43553915+K-Hanna@users.noreply.github.com>
2019-08-14 20:06:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random    
-square = [[0,0,0],[0,0,0],[0,0,0]]
+square = [['.','.','.'],['.','.','.'],['.','.','.']]
 
 def printSquare(a):
     for i in range(len(a)):
@@ -8,7 +8,7 @@ def printSquare(a):
         print()
         
 def add(x,y,z):
-    if square[x][y] == 0:
+    if square[x][y] == '.':
         square[x][y] = z
         printSquare(square)
         print()
@@ -16,42 +16,66 @@ def add(x,y,z):
     else:
         return 0
        
-def check(a,z):
-    if a[0][0] == z and a[1][1] == z and a[2][2] == z:
-            print('Player ' + str(z) + ' won.')
+def check(z):
+    if square[0][0] == z and square[1][1] == z and square[2][2] == z:
             return 9
-    elif a[0][0] == z and a[0][1] == z and a[0][2] == z:
-            print('Player ' + str(z) + ' won.')
+    elif square[0][0] == z and square[0][1] == z and square[0][2] == z:
             return 9
-    elif a[0][0] == z and a[1][0] == z and a[2][0] == z:
-            print('Player ' + str(z) + ' won.')
+    elif square[0][0] == z and square[1][0] == z and square[2][0] == z:
             return 9
-    elif a[1][1] == z and a[0][1] == z and a[2][1] == z:
-            print('Player ' + str(z) + ' won.')
+    elif square[1][1] == z and square[0][1] == z and square[2][1] == z:
             return 9
-    elif a[1][1] == z and a[1][0] == z and a[1][2] == z:
-            print('Player ' + str(z) + ' won.')
+    elif square[1][1] == z and square[1][0] == z and square[1][2] == z:
             return 9
-    elif a[1][1] == z and a[0][2] == z and a[2][0] == z:
-            print('Player ' + str(z) + ' won.')
+    elif square[1][1] == z and square[0][2] == z and square[2][0] == z:
             return 9
-    elif a[2][2] == z and a[2][0] == z and a[2][1] == z:
-            print('Player ' + str(z) + ' won.')
+    elif square[2][2] == z and square[2][0] == z and square[2][1] == z:
             return 9
-    elif a[2][2] == z and a[0][2] == z and a[1][2] == z:
-            print('Player ' + str(z) + ' won.')
+    elif square[2][2] == z and square[0][2] == z and square[1][2] == z:
             return 9
     else:
         return 0
 
-counter = 0
-while counter < 9:
-    if counter % 2 == 0:
-        x = random.randint(0,2)
-        y = random.randint(0,2)
-        counter = counter + add(x,y,1) + check(square,1)
-    else:
-        x = random.randint(0,2)
-        y = random.randint(0,2)
-        counter = counter + add(x,y,2) + check(square,2)
+def start(player1, player2):
+    counter = 0
+    while counter < 9:
+        if counter % 2 == 0:
+            x = random.randint(0,2)
+            y = random.randint(0,2)
+            counter = counter + add(x,y,player1) + check(player1)
+            if check(player1) == 9:
+                print('Player ' + player1 + ' won.')
+                print()
+        else:
+            x = random.randint(0,2)
+            y = random.randint(0,2)
+            counter = counter + add(x,y,player2) + check(player2)
+            if check(player2) == 9:
+                print('Player ' + player2 + ' won.')
+                print()
+    if counter == 9:
+        print('It is a tie.')
+        print()
+
+player1 = 'X'
+player2 = 'O'
+tries = 10
+games = 0
+playerX = 0
+playerO = 0
+while games < tries:
+    print('Game: ' + str(games + 1) + '.')
+    start(player1, player2)
+    if check(player1) == 9:
+        playerX += 1
+    if check(player2) == 9:
+        playerO += 1
+    games += 1
+    square = [['.','.','.'],['.','.','.'],['.','.','.']]
+tie = tries - playerX - playerO
+
+print()
+print('Player ' + player1 + ' won ' + str(playerX) + ' times.')
+print('Player ' + player2 + ' won ' + str(playerO) + ' times.')
+print('There were ' + str(tie) + ' ties.')
 
